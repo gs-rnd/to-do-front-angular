aa5917910353a341065f9100b560afc6809cd7f8
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-09 16:22:59 -0500

    created task-list view template

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Task } from './task.interface';
+
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+
+  toDoTasks: Task[] = [
+    { id: 1, title: 'Make To Do App', description: 'using Angular create a SPA for the front end.' },
+    { id: 2, title: 'Create component to display individual tasks' }
+  ];
 
   constructor() { }
 
