ad08a0f566d9829f0675364c18495ef9578bf0eb
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-09 16:04:07 -0500

    created task-list component

--- src/app/task-list/task-list.module.ts
+++ src/app/task-list/task-list.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
+import { TaskListComponent } from './task-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TaskListComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [TaskListComponent]
 })
 export class TaskListModule { }
