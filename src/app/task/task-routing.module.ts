96ec1791ab0d66c56170d496a6745e20f70ce7da
gerritsmith <63374003+gerritsmith@users.noreply.github.com>
2020-07-11 02:19:16 -0500

    created task-form component and registered route

--- src/app/task/task-routing.module.ts
+++ src/app/task/task-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 const routes: Routes = [
+  { path: 'task/new', component: TaskFormComponent },
   { path: 'task/:id', component: TaskDetailsComponent }
 ];
 
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { TaskRoutingModule } from './task-routing.module';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { TaskFormComponent } from './task-form/task-form.component';
 
 
 @NgModule({
-  declarations: [TaskDetailsComponent],
+  declarations: [TaskDetailsComponent, TaskFormComponent],
   imports: [
     CommonModule,
     TaskRoutingModule
